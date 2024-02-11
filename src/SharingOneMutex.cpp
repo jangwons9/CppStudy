e6f8d4974337dd16b613ac09da83516cc47de2a4
jangwons9 <jangwon.ss9@gmail.com>
2024-02-11 23:34:23 +0900

    sharing one mutex some understanding

--- src/SharingOneMutex.cpp
+++ src/SharingOneMutex.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <headertry.h>
+//#include <Eigen/Dense> just to try it worked~
+//using Eigen::MatrixXd;
 
 int SharedResource1 =0;
 int SharedResource2 =0;
@@ -10,21 +13,30 @@ void add1()
 {
     global.lock();
     SharedResource1++;
+    std::cout << "haha" << std::endl;
+    global.unlock();
+
+
 }
 
 void add2()
 {
     global.lock();
     SharedResource2++;
+    std::cout << "haha1212" << std::endl;
+    global.unlock();
+
 }
 
 int main()
-{   std::thread t1(add1);
+{   
+    std::thread t1(add1);
+    // adding t1.join() here makes sure t1 thread execute first
     std::thread t2(add2);
 
     t1.join();
     t2.join();
-    
+    // putting the t1join and t2join together like this make unsure which thread is excuted first
     std::cout << SharedResource1 << SharedResource2 << std::endl;
 }
     
